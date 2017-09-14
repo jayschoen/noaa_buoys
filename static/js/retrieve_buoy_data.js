5b6f389de4507b8553b8a34376e199e458463d0c
Jay Schoen <jay.r.schoen@gmail.com>
2017-09-14 14:55:13 -0400

    add unit toggle

--- static/js/retrieve_buoy_data.js
+++ static/js/retrieve_buoy_data.js
@@ -40,10 +40,39 @@ function ajax_get_buoy_data(station_id) {
 
 function add_data_to_dom(data) {
     console.log(data);
-    values_div = document.getElementById("values");
-    values_div.innerHTML = "";
-    for(let key in data.values) {
-        values_div.innerHTML += "<div>" + key + " "  + data.values[key] + " " + data.units[key]  + "</div></br>";
+    value_divs = {
+        "default_div": document.getElementById("default"),
+        "metric_div": document.getElementById("metric"),
+        "imperial_div": document.getElementById("imperial")
+    }
+    value_divs.default_div.innerHTML = "";
+    value_divs.metric_div.innerHTML = "";
+    value_divs.imperial_div.innerHTML = "";
+    for (let key in data.values) {
+        if (key === "imperial" || key === "metric") {
+            for (let subkey in data.values[key]) {
+                value_divs[key + "_div"].innerHTML += "<div>" + subkey + " " + data.values[key][subkey] + " " + data.units[key][subkey] + "</div></br>";
+            }
+        }
+        else {
+            value_divs.default_div.innerHTML += "<div>" + key + " "  + data.values[key] + " " + data.units[key]  + "</div></br>";
+        }
+    }
+}
+
+function toggle_units(selectObject) {
+    let type = selectObject.value;
+
+    let metric_div = document.getElementById("metric");
+    let imperial_div = document.getElementById("imperial");
+    if (type === "metric") {
+        metric_div.style = "display:unset;";
+        imperial_div.style = "display:none;";       
+    }
+    else {
+        metric_div.style = "display:none;";
+        imperial_div.style = "display:unset;";       
+
     }
 }
 
