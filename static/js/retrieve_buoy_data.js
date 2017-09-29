ce430dd50bdcf54da68d1807979708c2befa35b6
Jay Schoen <jay.r.schoen@gmail.com>
2017-09-29 15:03:06 -0400

    refactor station status to use dynamic id; improve performance with forkJoin(); add error catching

--- static/js/retrieve_buoy_data.js
+++ static/js/retrieve_buoy_data.js
@@ -1,51 +1,69 @@
-function get_station_status() {
-    
-    let station_status = Rx.Observable.fromPromise(
+function get_station_status(id) {
+
+    return Rx.Observable.fromPromise(
         fetch(
-            "http://127.0.0.1:5000/get-station-status/UP",
+            "http://127.0.0.1:5000/get-station-status/" + id,
             {
                 method: 'GET',
             }
         )
     )
     .flatMap(response => response.json())
-    .subscribe(
-        response => update_station_status_dom(response),
-        error => console.log(error),
-        () => console.log('completed')   
-    );
+    .map(response => {
+        response.id = id
+        return response;    
+    })
+    .catch(error => Rx.Observable.of({"id": id, "error": "Bad Response"}));
 
 }
 
-function update_station_status_dom(status_obj) {
+function update_station_status_dom(status_obj, list_mode) {
     
     let status_val = "";
-    if (status_obj.result === 0) {
-        status_val = "&uarr;";
-        status_color = "#42f495";
-    }
-    else {
-        status_val = "&darr;";
-        status_color = "#f4425c";
-    }
+    if (status_obj.error === null) {
+        if (status_obj.result === 0) {
+            status_val = "&uarr;";
+            status_color = "#42f495";
+        }
+        else {
+            status_val = "&darr;";
+            status_color = "#f4425c";
+        }
+    
+        let selector_val = "";
+        if (list_mode === true) {
+            selector_val = "station_status_" + status_obj.id;
+        }
+        else {
+            selector_val = "station_status";
+        }
 
-    let selector = document.getElementById("station_status");
+        let selector = document.getElementById(selector_val);
 
-    selector.innerHTML = "<div class='status-indicator rounded-circle' style='background-color:" + status_color + ";'>" + status_val + "</div>"; 
+        selector.innerHTML = "<div class='status-indicator rounded-circle' style='background-color:" + status_color + ";'>" + status_val + "</div>"; 
+    }
 
 }
 
 function get_buoy_data() {
-
+    
     let selector = document.getElementById("station_selector");
 
     let station_selection = Rx.Observable.fromEvent(selector, "change")
     .map(event => event.target.value)
     .startWith("UP")
-    .flatMap(station_id => ajax_get_buoy_data(station_id));
+    .flatMap(station_id => {
+        return Rx.Observable.forkJoin(
+            ajax_get_buoy_data(station_id),
+            get_station_status(station_id)
+        );
+    });
 
     station_selection.subscribe(
-        response => add_data_to_dom(response),
+        response => {
+            add_data_to_dom(response[0]);
+            update_station_status_dom(response[1]);
+        },
         error => console.log(error),
         () => console.log('completed')
     );
@@ -72,38 +90,48 @@ function ajax_get_buoy_data(station_id) {
             return response;
         }
     })
-    .flatMap(response => response.json());
+    .flatMap(response => response.json())
+    .catch(error => Rx.Observable.of({"id": station_id, "error": "Bad Response"})); 
 
 }
 
 function add_data_to_dom(data) {
-    value_divs = {
-        "metric_div": document.getElementById("metric"),
-        "imperial_div": document.getElementById("imperial")
+
+    if (data.hasOwnProperty('error') && data.error !== null) {
+        
+        document.getElementById("error").innerHTML = "Unable to retrieve buoy data.";
+
     }
-    value_divs.metric_div.innerHTML = "";
-    value_divs.imperial_div.innerHTML = "";
+    else {
 
-    let selected_unit = document.getElementById('unit_selector').value;
-
-    let unit;
-    for (let key in data.values) {
-
-        if (key === "imperial" || key === "metric") {
-            for (let subkey in data.values[key]) {
-                unit = data.units[key][subkey];
-                value_divs[key + "_div"].innerHTML += "<div class='row table-row-margin'><div class='col-8'>" + _xform_keys(subkey) + "</div><div class='col-4'>" + data.values[key][subkey] + unit + "</div></div>";
-            }
+        value_divs = {
+            "metric_div": document.getElementById("metric"),
+            "imperial_div": document.getElementById("imperial")
         }
-        else {
-            
-            unit = data.units[key];
-            if (unit === 'direction') {
-                unit = "";
-            }
-            value_divs.metric_div.innerHTML += "<div class='row table-row-margin'><div class='col-8'>" + _xform_keys(key) + "</div><div class='col-4'>"  + data.values[key] + unit  + "</div></div>";
-            value_divs.imperial_div.innerHTML += "<div class='row table-row-margin'><div class='col-8'>" + _xform_keys(key) + "</div><div class='col-4'>"  + data.values[key] + unit  + "</div></div>";
+        value_divs.metric_div.innerHTML = "";
+        value_divs.imperial_div.innerHTML = "";
 
+        let selected_unit = document.getElementById('unit_selector').value;
+    
+        let unit;
+        for (let key in data.values) {
+
+            if (key === "imperial" || key === "metric") {
+                for (let subkey in data.values[key]) {
+                    unit = data.units[key][subkey];
+                    value_divs[key + "_div"].innerHTML += "<div class='row table-row-margin'><div class='col-8'>" + _xform_keys(subkey) + "</div><div class='col-4'>" + data.values[key][subkey] + unit + "</div></div>";
+                }
+            }
+            else {
+                
+                unit = data.units[key];
+                if (unit === 'direction') {
+                    unit = "";
+                }
+                value_divs.metric_div.innerHTML += "<div class='row table-row-margin'><div class='col-8'>" + _xform_keys(key) + "</div><div class='col-4'>"  + data.values[key] + unit  + "</div></div>";
+                value_divs.imperial_div.innerHTML += "<div class='row table-row-margin'><div class='col-8'>" + _xform_keys(key) + "</div><div class='col-4'>"  + data.values[key] + unit  + "</div></div>";
+
+            }
         }
     }
 }
@@ -153,7 +181,25 @@ function _xform_keys(key) {
     return modified_keys;  
 }
 
+function _build_station_list() {
+    let stations = [ "AN", "FL", "GR", "J", "N", "PL", "RC", "S", "SN", "SR", "UP" ];
+
+    let station_observables = stations.map(station_id => get_station_status(station_id));
+
+    let obs = Rx.Observable.forkJoin(station_observables)
+
+    let station_list = document.getElementById('station-list-container');
+
+    obs.subscribe(id_list => {
+        id_list.map(response => {
+            station_list.innerHTML += "<div class='row'><div class='col'><div class='status-label'>" + response.id + "</div></div><div class='col'><div id='station_status_" + response.id + "' " + "class='status-indicator2 rounded-circle'>X</div></div></div>";
+            update_station_status_dom(response, true);
+        });
+    });
+
+}
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     get_buoy_data();
-    get_station_status();
+    _build_station_list();
 });
